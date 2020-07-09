11fe2ecbe44364ac3214087a8ba979b9491a92d4
Lait <mr.lait3@gmail.com>
2020-07-09 17:00:21 +0300

    created a new project.

---
+++ Task01/Task1.1/src/GCDAlgorithm/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace GCDAlgorithm
+{
+    public class Class1
+    {
+        decimal a = Math.Abs(10);
+        Math Math = new Math();
+    }
+}
