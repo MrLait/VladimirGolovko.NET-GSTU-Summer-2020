927f1430decf8a8ae8662adb3935ce303180123b
Lait <mr.lait3@gmail.com>
2020-07-13 15:27:20 +0300

    Added binaryGcd calculation for two, three and four numbers and tests for them.

--- Task01/Tests/GCDAlgorithmTests/CalculationOfGCDTest.cs
+++ Task01/Tests/GCDAlgorithmTests/CalculationOfGCDTest.cs
@@ -50,7 +50,7 @@ namespace GCDAlgorithmTests
             //Assert
             Assert.AreEqual(expectedResult, actualEuclidGcd);
         }
-        
+
         [TestCase(1, 2, 1, 1)]
         [TestCase(10, 0, 10, 10)]
         [TestCase(100000, 27, 1, 1)]
@@ -95,7 +95,7 @@ namespace GCDAlgorithmTests
             //Assert
             Assert.AreEqual(expectedResult, actualEuclidGcd);
         }
-        
+
         [TestCase(1, 2, 1, 1, 1)]
         [TestCase(10, 0, 10, 10, 10)]
         [TestCase(100000, 27, 1, 1, 1)]
@@ -140,5 +140,142 @@ namespace GCDAlgorithmTests
             //Assert
             Assert.AreEqual(expectedResult, actualEuclidGcd);
         }
+
+
+        [TestCase(1, 2, 1)]
+        [TestCase(10, 0, 10)]
+        [TestCase(100000, 27, 1)]
+        [TestCase(100000, 28, 4)]
+        [TestCase(16, 100000, 16)]
+        public void GetBinaryGcd_ForTwoNubmers_PositiveNumbers(int numOne, int numTwo, int expectedResult)
+        {
+            //Arrange
+            int actualBinaryGcd;
+            //Act
+            actualBinaryGcd = CalculationOfGCD.GetBinaryGcd(numOne, numTwo);
+            //Assert
+            Assert.AreEqual(expectedResult, actualBinaryGcd);
+        }
+
+        [TestCase(-1, -2, 1)]
+        [TestCase(-10, 0, 10)]
+        [TestCase(-100000, -27, 1)]
+        [TestCase(-100000, -28, 4)]
+        [TestCase(-16, -100000, 16)]
+        public void GetBinaryGcd_ForTwoNubmers_NegativeNumbars(int numOne, int numTwo, int expectedResult)
+        {
+            //Arrange
+            int actualBinaryGcd;
+            //Act
+            actualBinaryGcd = CalculationOfGCD.GetBinaryGcd(numOne, numTwo);
+            //Assert
+            Assert.AreEqual(expectedResult, actualBinaryGcd);
+        }
+
+        [TestCase(-1, 2, 1)]
+        [TestCase(-10, 0, 10)]
+        [TestCase(100000, -27, 1)]
+        [TestCase(-100000, 28, 4)]
+        [TestCase(16, -100000, 16)]
+        public void GetBinaryGcd_ForTwoNubmers_PositiveAndNegativeNumbers(int numOne, int numTwo, int expectedResult)
+        {
+            //Arrange
+            int actualBinaryGcd;
+            //Act
+            actualBinaryGcd = CalculationOfGCD.GetBinaryGcd(numOne, numTwo);
+            //Assert
+            Assert.AreEqual(expectedResult, actualBinaryGcd);
+        }
+
+        [TestCase(1, 2, 1, 1)]
+        [TestCase(10, 0, 10, 10)]
+        [TestCase(100000, 27, 1, 1)]
+        [TestCase(100000, 28, 4, 4)]
+        [TestCase(16, 100000, 16, 16)]
+        public void GetBinaryGcd_ForThreeNubmers_PositiveNumbers(int numOne, int numTwo, int numThree, int expectedResult)
+        {
+            //Arrange
+            int actualBinaryGcd;
+            //Act
+            actualBinaryGcd = CalculationOfGCD.GetBinaryGcd(numOne, numTwo, numThree);
+            //Assert
+            Assert.AreEqual(expectedResult, actualBinaryGcd);
+        }
+
+        [TestCase(-1, -2, -1, 1)]
+        [TestCase(-10, 0, -10, 10)]
+        [TestCase(-100000, -27, -1, 1)]
+        [TestCase(-100000, -28, -4, 4)]
+        [TestCase(-16, -100000, -16, 16)]
+        public void GetBinaryGcd_ForThreeNubmer_NegativeNumbers(int numOne, int numTwo, int numThree, int expectedResult)
+        {
+            //Arrange
+            int actualBinaryGcd;
+            //Act
+            actualBinaryGcd = CalculationOfGCD.GetBinaryGcd(numOne, numTwo, numThree);
+            //Assert
+            Assert.AreEqual(expectedResult, actualBinaryGcd);
+        }
+
+        [TestCase(-1, 2, 1, 1)]
+        [TestCase(-10, 0, 10, 10)]
+        [TestCase(100000, -27, 1, 1)]
+        [TestCase(-100000, 28, 4, 4)]
+        [TestCase(16, -100000, 16, 16)]
+        public void GetBinaryGcd_ForThreeNubmer_PositiveAndNegativeNumbers(int numOne, int numTwo, int numThree, int expectedResult)
+        {
+            //Arrange
+            int actualBinaryGcd;
+            //Act
+            actualBinaryGcd = CalculationOfGCD.GetBinaryGcd(numOne, numTwo, numThree);
+            //Assert
+            Assert.AreEqual(expectedResult, actualBinaryGcd);
+        }
+
+        [TestCase(1, 2, 1, 1, 1)]
+        [TestCase(10, 0, 10, 10, 10)]
+        [TestCase(100000, 27, 1, 1, 1)]
+        [TestCase(100000, 28, 4, 4, 4)]
+        [TestCase(16, 100000, 16, 16, 16)]
+        public void GetBinaryGcd_ForFourNubmers_PositiveNumbers(int numOne, int numTwo, int numThree, int numFour, int expectedResult)
+        {
+            //Arrange
+            int actualBinaryGcd;
+            //Act
+            actualBinaryGcd = CalculationOfGCD.GetBinaryGcd(numOne, numTwo, numThree, numFour);
+            //Assert
+            Assert.AreEqual(expectedResult, actualBinaryGcd);
+        }
+
+        [TestCase(-1, -2, -1, -1, 1)]
+        [TestCase(-10, 0, -10, -10, 10)]
+        [TestCase(-100000, -27, -1, -1, 1)]
+        [TestCase(-100000, -28, -4, -4, 4)]
+        [TestCase(-16, -100000, -16, -16, 16)]
+        public void GetBinaryGcd_ForFourNubmers_NegativeNumbers(int numOne, int numTwo, int numThree, int numFour, int expectedResult)
+        {
+            //Arrange
+            int actualBinaryGcd;
+            //Act
+            actualBinaryGcd = CalculationOfGCD.GetBinaryGcd(numOne, numTwo, numThree, numFour);
+            //Assert
+            Assert.AreEqual(expectedResult, actualBinaryGcd);
+        }
+
+        [TestCase(-1, 2, 1, 1, 1)]
+        [TestCase(-10, 0, 10, 10, 10)]
+        [TestCase(100000, -27, 1, 1, 1)]
+        [TestCase(-100000, 28, 4, 4, 4)]
+        [TestCase(16, -100000, 16, 16, 16)]
+        public void GetBinaryGcd_ForFourNubmers_PositiveAndNegativeNumbers(int numOne, int numTwo, int numThree, int numFour, int expectedResult)
+        {
+            //Arrange
+            int actualBinaryGcd;
+            //Act
+            actualBinaryGcd = CalculationOfGCD.GetBinaryGcd(numOne, numTwo, numThree, numFour);
+            //Assert
+            Assert.AreEqual(expectedResult, actualBinaryGcd);
+        }
+
     }
 }
--- Task01/src/GCDAlgorithm/CalculationOfGCD.cs
+++ Task01/src/GCDAlgorithm/CalculationOfGCD.cs
@@ -35,5 +35,64 @@ namespace GCDAlgorithm
             return Math.Abs(numOne);
         }
 
+        public static int GetBinaryGcd(int numOne, int numTwo)
+        {
+            int binaryGcdForTwoNum = GetBinaryGcdForTwoNum(numOne, numTwo);
+            return binaryGcdForTwoNum;
+        }
+
+        public static int GetBinaryGcd(int numOne, int numTwo, int numThree)
+        {
+            int binaryGcdForTwoNum = GetBinaryGcdForTwoNum(numOne, numTwo);
+            int binaryGcdForThreeNum = GetBinaryGcdForTwoNum(binaryGcdForTwoNum, numThree);
+            return binaryGcdForThreeNum;
+        }
+
+        public static int GetBinaryGcd(int numOne, int numTwo, int numThree, int numFour)
+        {
+            int binaryGcdForTwoNum = GetBinaryGcdForTwoNum(numOne, numTwo);
+            int binaryGcdForThreeNum = GetBinaryGcdForTwoNum(binaryGcdForTwoNum, numThree);
+            int binaryGcdForFourNum = GetBinaryGcdForTwoNum(binaryGcdForThreeNum, numFour);
+            return binaryGcdForFourNum;
+        }
+
+        private static int GetBinaryGcdForTwoNum(int numbOne, int numbTwo)
+        {
+            numbOne = Math.Abs(numbOne);
+            numbTwo = Math.Abs(numbTwo);
+
+            if (numbOne == 0)
+                return numbTwo;
+
+            if (numbTwo == 0)
+                return numbOne;
+
+            int k;
+
+            for (k = 0; ((numbOne | numbTwo) & 1) == 0; ++k)
+            {
+                numbOne >>= 1;
+                numbTwo >>= 1;
+            }
+
+            while ((numbOne & 1) == 0)
+                numbOne >>= 1;
+            do
+            {
+                while ((numbTwo & 1) == 0)
+                    numbTwo >>= 1;
+                if (numbOne > numbTwo)
+                {
+                    int temp = numbOne;
+                    numbOne = numbTwo;
+                    numbTwo = temp;
+                }
+                numbTwo = (numbTwo - numbOne);
+            } 
+            while (numbTwo != 0);
+
+            return numbOne << k;
+        }
+
     }
 }
