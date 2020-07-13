d6a039e7111cb31a16723fba8bbcc2c112114848
Lait <mr.lait3@gmail.com>
2020-07-13 12:36:47 +0300

    1. Разработать тип, реализующий алгоритм Евклида для вычисления НОД двух целых чисел - is completed Unit tests for testing the method(GetEuclidGcd for two numbers) are also added.

---
+++ Task01/Tests/GCDAlgorithmTests/CalculationOfGCDTest.cs
@@ -0,0 +1,54 @@
+﻿using GCDAlgorithm;
+using NUnit.Framework;
+
+namespace GCDAlgorithmTests
+{
+    [TestFixture()]
+    public class CalculationOfGCDTest
+    {
+        [TestCase(1, 2, 1)]
+        [TestCase(10, 0, 10)]
+        [TestCase(100000, 27, 1)]
+        [TestCase(100000, 28, 4)]
+        [TestCase(16, 100000, 16)]
+        public void GetEuclidGcd_ForTwoNubmers_PositiveNumbers(int numOne, int numTwo, int expectedResult)
+        {
+            //Arrange
+            int actualEuclidGcd;
+            //Act
+            actualEuclidGcd = CalculationOfGCD.GetEuclidGcd(numOne, numTwo);
+            //Assert
+            Assert.AreEqual(expectedResult, actualEuclidGcd);
+        }
+
+        [TestCase(-1, -2, 1)]
+        [TestCase(-10, 0, 10)]
+        [TestCase(-100000, -27, 1)]
+        [TestCase(-100000, -28, 4)]
+        [TestCase(-16, -100000, 16)]
+        public void GetEuclidGcd_ForTwoNubmers_PositiveNegative(int numOne, int numTwo, int expectedResult)
+        {
+            //Arrange
+            int actualEuclidGcd;
+            //Act
+            actualEuclidGcd = CalculationOfGCD.GetEuclidGcd(numOne, numTwo);
+            //Assert
+            Assert.AreEqual(expectedResult, actualEuclidGcd);
+        }
+
+        [TestCase(-1, 2, 1)]
+        [TestCase(-10, 0, 10)]
+        [TestCase(100000, -27, 1)]
+        [TestCase(-100000, 28, 4)]
+        [TestCase(16, -100000, 16)]
+        public void GetEuclidGcd_ForTwoNubmers_PositiveAndNegative(int numOne, int numTwo, int expectedResult)
+        {
+            //Arrange
+            int actualEuclidGcd;
+            //Act
+            actualEuclidGcd = CalculationOfGCD.GetEuclidGcd(numOne, numTwo);
+            //Assert
+            Assert.AreEqual(expectedResult, actualEuclidGcd);
+        }
+    }
+}
---
+++ Task01/src/GCDAlgorithm/CalculationOfGCD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace GCDAlgorithm
+{
+    public static class CalculationOfGCD
+    {
+        public static int GetEuclidGcd(int numOne, int numTwo)
+        {
+            int GcdForTwoNumbers = GetEuclidGcdForTwoNumbers(numOne, numTwo);
+            return GcdForTwoNumbers;
+        }
+
+        private static int GetEuclidGcdForTwoNumbers(int numOne, int numTwo)
+        {
+            while (numTwo != 0)
+                numTwo = numOne % (numOne = numTwo);
+            return Math.Abs(numOne);
+        }
+
+    }
+}
