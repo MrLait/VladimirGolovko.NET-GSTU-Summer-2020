2f94a8c78c4b23bd0ec314e1714f590a24ef0b1f
Lait <mr.lait3@gmail.com>
2020-07-14 16:59:51 +0300

    A class has been created that allows loading data from a text file into an array and tests for it.

---
+++ Task01/Tests/ReaderIntoArrayTests/ReadDataFromTxtFileTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using ReaderIntoArray.Model;
+using ReaderIntoArray.Repositories;
+
+namespace ReaderIntoArray.Tests
+{
+    [TestFixture()]
+    public class ReadDataFromTxtFileTests
+    {
+        private string correctPathToTxtFile = @"src\ReaderIntoArray\txtFile\figures.txt";
+        private string notValidPathToTxtFile = @"src\ReaderIntoArray\txtFile\figuress.txt";
+
+        [TestCase()]
+        public void GivenStringsProperty_WhenPathtToFileIsTrue_ThenOutIsTrue()
+        {
+            //Arrange
+            ConvertStringToArray convertStringToArr = new ConvertStringToArray(new ReadDataFromTxtFileRepository(correctPathToTxtFile));
+            //Act
+            var actualArray = convertStringToArr.Strings;
+            string[] expectedResult = new string[] { "Rectangle; 10; 20;", "Square; 5;", "Circle; 30;" };
+            //Assert
+            Assert.AreEqual(expectedResult, actualArray);
+        }
+
+        [TestCase()]
+        public void GivenStringsProperty_WhenPathtToFileIsNotValid_ThenOutIsFileNotFoundException()
+        {
+            //Assert
+            Assert.That(() => new ConvertStringToArray(new ReadDataFromTxtFileRepository(notValidPathToTxtFile)), Throws.TypeOf<System.IO.FileNotFoundException>());
+        }
+    }
+}
---
+++ Task01/src/ReaderIntoArray/Repositories/ReadDataFromTxtFileRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace ReaderIntoArray.Repositories
+{
+    public class ReadDataFromTxtFileRepository : IReaderRepository<string>
+    {
+        public string PathToFile { get; private set; }
+
+		public ReadDataFromTxtFileRepository(string pathToFile)
+        {
+            PathToFile = pathToFile;
+        }
+
+        public string GetAll()
+        {
+            string textFromFile = string.Empty;
+            
+            try
+            {
+                using (FileStream fstream = File.OpenRead(PathToFile))
+                {
+                    // преобразуем строку в байты
+                    byte[] array = new byte[fstream.Length];
+                    fstream.Read(array, 0, array.Length);
+                    textFromFile = System.Text.Encoding.Default.GetString(array);
+                }
+            }
+			catch (FileNotFoundException)
+			{
+				throw new FileNotFoundException("The file with path: " + PathToFile + " is not found");
+			}
+            return textFromFile;
+        }
+    }
+}
