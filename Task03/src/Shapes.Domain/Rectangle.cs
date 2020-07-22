9c7c23c7fd9eca0809181d189492cdf00125d059
Lait <mr.lait3@gmail.com>
2020-07-22 14:54:12 +0300

    task3.1 init and copied models from the previous task

---
+++ Task03/src/Shapes.Domain/Rectangle.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Shapes.Domain
+{
+    /// <summary>
+    ///  Class rectangle shape.
+    /// </summary>
+    public class Rectangle : BaseShape
+    {
+        /// <summary>
+        /// Constructor with length and width parameters.
+        /// </summary>
+        /// <param name="length">Length param.</param>
+        /// <param name="width">Width param.</param>
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        /// <summary>
+        /// Property length
+        /// </summary>
+        public double Length { get; private set; }
+
+        /// <summary>
+        /// Property width
+        /// </summary>
+        public double Width { get; private set; }
+
+        /// <summary>
+        /// Property to getting area of rectangle.
+        /// </summary>
+        public override double Area
+        {
+            get => Length * Width;
+        }
+
+        /// <summary>
+        /// Property to getting perimeter of rectangle. 
+        /// </summary>
+        public override double Perimeter
+        {
+            get => 2 * Length + 2 * Width;
+        }
+
+        /// <summary>
+        /// Comparing one rectangle with another.
+        /// </summary>
+        /// <param name="obj">The compared rectangle.</param>
+        /// <returns>True if equal. False if not equal.</returns>
+        public override bool Equals(Object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            Rectangle r = (Rectangle)obj;
+            return Length.Equals(r.Length) && Width.Equals(r.Width);
+        }
+
+        /// <summary>
+        /// Calculate hash code.
+        /// </summary>
+        /// <returns>The total hesh code.</returns>
+        public override int GetHashCode()
+        {
+            return Length.GetHashCode() + Width.GetHashCode();
+        }
+
+        /// <summary>
+        /// Represents class members in string format.
+        /// </summary>
+        /// <returns>Returns class members in string format.</returns>
+        public override string ToString()
+        {
+            return string.Format("{0};{1};{2}", base.ToString(), Length, Width);
+        }
+    }
+}
---
+++ Task03/src/Shapes.Domain/Square.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Shapes.Domain
+{
+    /// <summary>
+    ///  Class square shape.
+    /// </summary>
+    public class Square : BaseShape
+    {
+        /// <summary>
+        /// Constructor with length parameter.
+        /// </summary>
+        /// <param name="length"></param>
+        public Square(double length)
+        {
+            Side = length;
+        }
+        /// <summary>
+        /// Property side
+        /// </summary>
+        public double Side { get; private set; }
+
+        /// <summary>
+        /// Property to getting area of square.
+        /// </summary>
+        public override double Area
+        {
+            get => Math.Pow(Side, 2);
+        }
+
+        /// <summary>
+        /// Property to getting perimeter of square.
+        /// </summary>
+        public override double Perimeter
+        {
+            get => Side * 4;
+        }
+
+        /// <summary>
+        /// Comparing one square with another.
+        /// </summary>
+        /// <param name="obj">The compared square.</param>
+        /// <returns>True if equal. False if not eqal.</returns>
+        public override bool Equals(Object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            Square r = (Square)obj;
+            return Side.Equals(r.Side);
+        }
+
+        /// <summary>
+        /// Calculate hash code.
+        /// </summary>
+        /// <returns>The total hesh code.</returns>
+        public override int GetHashCode()
+        {
+            return Side.GetHashCode();
+        }
+
+        /// <summary>
+        /// Represents class members in string format.
+        /// </summary>
+        /// <returns>Returns class members in string format.</returns>
+        public override string ToString()
+        {
+            return string.Format("{0};{1}", base.ToString(), Side);
+        }
+    }
+}
