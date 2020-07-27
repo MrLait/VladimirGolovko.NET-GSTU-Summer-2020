bd45f68cfed6b4e08c26721242d81c61d89a46c5
Lait <mr.lait3@gmail.com>
2020-07-27 17:55:04 +0300

    Changed assamblyInfo for Shapes.Data

--- Task03/src/Shapes.Data/Properties/AssemblyInfo.cs
+++ Task03/src/Shapes.Data/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Girls.Data")]
+[assembly: AssemblyTitle("Shapes.Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Girls.Data")]
+[assembly: AssemblyProduct("Shapes.Data")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
