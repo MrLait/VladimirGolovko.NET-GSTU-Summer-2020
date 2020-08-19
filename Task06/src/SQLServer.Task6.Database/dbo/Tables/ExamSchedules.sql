6c44003ae1733a34dc99018a093884d613578123
Lait <mr.lait3@gmail.com>
2020-08-19 15:56:07 +0300

    SQLServer.Task6.Database and tables are implemented.

---
+++ Task06/src/SQLServer.Task6.Database/dbo/Tables/ExamSchedules.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ExamSchedules]
+(
+    [Id]            INT IDENTITY(1,1)   PRIMARY KEY,
+    [SessionsId]    INT                 NOT NULL,
+    [GroupsId]      INT                 NOT NULL,
+    [SubjectsId]    INT                 NOT NULL,
+    [ExamDate]      DATETIME            NOT NULL,
+    CONSTRAINT [FK_ExamSchedules_Sessions_Id] FOREIGN KEY ([SessionsId]) REFERENCES [Sessions]([Id]), 
+    CONSTRAINT [FK_ExamSchedules_Groups_Id] FOREIGN KEY ([GroupsId]) REFERENCES [Groups]([Id]), 
+    CONSTRAINT [FK_ExamSchedules_Subjects_Id] FOREIGN KEY ([SubjectsId]) REFERENCES [Subjects]([Id])
+)
