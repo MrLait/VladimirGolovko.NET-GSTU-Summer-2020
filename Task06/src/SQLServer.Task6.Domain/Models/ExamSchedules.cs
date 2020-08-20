76d2072b42d7ab58a6069bc1e50f929b4976d88f
Lait <mr.lait3@gmail.com>
2020-08-20 14:11:02 +0300

    Added table models.

---
+++ Task06/src/SQLServer.Task6.Domain/Models/ExamSchedules.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLServer.Task6.Domain.Models
+{
+    public class ExamSchedules
+    {
+        public int Id { get; set; }
+        public int SessionsId { get; set; }
+        public int GroupsId { get; set; }
+        public int SubjectsId { get; set; }
+        public DateTime ExamDate { get; set; }
+    }
+}
---
+++ Task06/src/SQLServer.Task6.Domain/Models/Students.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLServer.Task6.Domain.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirthday { get; set; }
+        public int GroupsID { get; set; }
+    }
+}
