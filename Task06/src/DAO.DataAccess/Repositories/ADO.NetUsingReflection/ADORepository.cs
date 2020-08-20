1fff72779d1a47c0319592bb0515f7daf9eb05c8
Lait <mr.lait3@gmail.com>
2020-08-20 15:08:10 +0300

    Added data access layer and crud methodts

---
+++ Task06/src/DAO.DataAccess/Repositories/ADO.NetUsingReflection/ADORepository.cs
@@ -0,0 +1,35 @@
+﻿using DAO.DataAccess.Interfaces;
+using SQLServer.Task6.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace DAO.DataAccess.Repositories.ADO.NetUsingReflection
+{
+    public class ADORepository<T> : ICRUD<T> where T : IEntity, new()
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int byID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByID(int byID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
