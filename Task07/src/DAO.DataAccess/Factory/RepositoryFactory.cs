2a771c3392556f5d3d9a22d31941dbdad41edd1e
Lait <mr.lait3@gmail.com>
2020-08-31 12:16:33 +0300

    Added DAO.DataAccess from task6 and updated factory with sinleton

---
+++ Task07/src/DAO.DataAccess/Factory/RepositoryFactory.cs
@@ -0,0 +1,116 @@
+﻿using DAO.DataAccess.Interfaces;
+using DAO.DataAccess.Repositories.ADONetUsingReflection;
+using SQLServer.Task7.Domain.Models;
+
+namespace DAO.DataAccess.Factory
+{
+    /// <summary>
+    /// ADO repository factory class.
+    /// </summary>
+    public class ADORepositoryFactory : AbstractFactory
+    {
+        /// <summary>
+        /// Represent access to ExamSchedules table.
+        /// </summary>
+        protected internal Repository<ExamSchedules> ExamSchedules { get;  set; }
+
+        /// <summary>
+        /// Represent access to Groups.
+        /// </summary>
+        protected internal Repository<Groups> Groups { get;  set; }
+
+        /// <summary>
+        /// Represent access to Sessions.
+        /// </summary>
+        protected internal Repository<Sessions> Sessions { get;  set; }
+
+        /// <summary>
+        /// Represent access to SessionsResults.
+        /// </summary>
+        protected internal Repository<SessionsResults> SessionsResults { get;  set; }
+
+        /// <summary>
+        /// Represent access to Students.
+        /// </summary>
+        protected internal Repository<Students> Students { get;  set; }
+
+        /// <summary>
+        /// Represent access to Subjects.
+        /// </summary>
+        protected internal Repository<Subjects> Subjects { get;  set; }
+
+        /// <summary>
+        /// Represent access to Examiners.
+        /// </summary>
+        protected internal Repository<Examiners> Examiners { get; set; }
+
+        /// <summary>
+        /// Represent access to Specialties.
+        /// </summary>
+        protected internal Repository<Specialties> Specialties { get; set; }
+
+        /// <summary>
+        /// Constructor to initializing access with tables and get connection string to database.
+        /// </summary>
+        /// <param name="dbConnectionStrig"></param>
+        public ADORepositoryFactory(string dbConnectionStrig)
+        {
+            ExamSchedules = new Repository<ExamSchedules>(dbConnectionStrig);
+            Groups = new Repository<Groups>(dbConnectionStrig);
+            Sessions = new Repository<Sessions>(dbConnectionStrig);
+            SessionsResults = new Repository<SessionsResults>(dbConnectionStrig);
+            Students = new Repository<Students>(dbConnectionStrig);
+            Subjects = new Repository<Subjects>(dbConnectionStrig);
+            Examiners = new Repository<Examiners>(dbConnectionStrig);
+            Specialties = new Repository<Specialties>(dbConnectionStrig);
+        }
+
+        /// <summary>
+        /// Factory method to create ExamSchedules table.
+        /// </summary>
+        /// <returns>Returns ExamSchedules.</returns>
+        public override ICRUD<ExamSchedules> CreateExamSchedules() => ExamSchedules;
+
+        /// <summary>
+        /// Factory method to create Groups table.
+        /// </summary>
+        /// <returns>Returns Groups.</returns>
+        public override ICRUD<Groups> CreateGroups() => Groups;
+
+        /// <summary>
+        /// Factory method to create Sessions table.
+        /// </summary>
+        /// <returns>Returns Sessions.</returns>
+        public override ICRUD<Sessions> CreateSessions() => Sessions;
+
+        /// <summary>
+        /// Factory method to create SessionsResults table.
+        /// </summary>
+        /// <returns>Returns SessionsResults.</returns>
+        public override ICRUD<SessionsResults> CreateSessionsResults() => SessionsResults;
+
+        /// <summary>
+        /// Factory method to create Students table.
+        /// </summary>
+        /// <returns>Returns Students.</returns>
+        public override ICRUD<Students> CreateStudents() => Students;
+
+        /// <summary>
+        /// Factory method to create Subjects table.
+        /// </summary>
+        /// <returns>Returns Subjects.</returns>
+        public override ICRUD<Subjects> CreateSubjects() => Subjects;
+
+        /// <summary>
+        /// Factory method to create Examiners table.
+        /// </summary>
+        /// <returns>Returns Examiners.</returns>
+        public override ICRUD<Examiners> CreateExaminers() => Examiners;
+
+        /// <summary>
+        /// Factory method to create Specialties table.
+        /// </summary>
+        /// <returns>Returns Specialties.</returns>
+        public override ICRUD<Specialties> CreateSpecialties() => Specialties;
+    }
+}
---
+++ Task07/src/DAO.DataAccess/Repositories/LINQtoSQLRepository/Repository.cs
@@ -0,0 +1,272 @@
+﻿using DAO.DataAccess.Interfaces;
+using DAO.DataAccess.Repositories.Exstension;
+using SQLServer.Task7.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+
+namespace DAO.DataAccess.Repositories.LINQtoSQLRepository
+{
+    /// <summary>
+    /// Abstract repository with implemented CRUD methods.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Repository<T> : ICRUD<T> where T : IEntity, new()
+    {
+        /// <summary>
+        /// Connection string to database
+        /// </summary>
+        protected string DbConString { get; private set; }
+
+        /// <summary>
+        /// Constructor <see cref="Repository{T}"/>
+        /// </summary>
+        /// <param name="dbConString"><see cref="DbConString"/></param>
+        public Repository(string dbConString)
+        {
+            DbConString = dbConString;
+        }
+
+        /// <summary>
+        /// Implementation Add <see cref="ICRUD{T}.Add(T)"/>
+        /// </summary>
+        /// <param name="entity">Models.</param>
+        public void Add(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(typeof(T).Name + "object Should not be Null when Saving to database");
+
+            var storedProcedure = "Add" + entity.GetType().Name;
+
+            using (SqlConnection sqlConnection = new SqlConnection(DbConString))
+            {
+                SqlCommand sqlCommand = SqlCommandInstance(storedProcedure, sqlConnection); //using
+                
+                try
+                {
+                    sqlCommand.Parameters.AddRange(GetAddParameter(entity).ToArray());
+                    sqlConnection.Open();
+                    sqlCommand.ExecuteScalar();
+                }
+                catch (SqlException sqlEx)
+                {
+                    throw new ArgumentException("Some Error occured at database, if error in stored procedure: " + storedProcedure, sqlEx);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Implementation Delete <see cref="ICRUD{T}.Delete(int)"/>
+        /// </summary>
+        /// <param name="byId">Id.</param>
+        public void Delete(int byId)
+        {
+            if (byId == 0)
+                throw new ArgumentNullException("byId should not be 0");
+
+            string tableName = new T().GetType().Name;
+            string storedProcedure = "Delete" + tableName + "ById";
+
+            using (SqlConnection sqlConnection = new SqlConnection(DbConString))
+            {
+                SqlCommand sqlCommand = SqlCommandInstance(storedProcedure, sqlConnection, new SqlParameter[] { new SqlParameter("Id", byId) });
+
+                try
+                {
+                    sqlConnection.Open();
+                    sqlCommand.ExecuteNonQuery();
+                }
+                catch (SqlException sqlEx)
+                {
+                    throw new ArgumentException("Some Error occured at database, if error in stored procedure: " + storedProcedure, sqlEx);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Implementation GetAll <see cref="ICRUD{T}.GetAll"/>
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> GetAll()
+        {
+            string tableName = new T().GetType().Name;
+            string storedProcedure = "GetAll" + tableName;
+
+            using (SqlConnection sqlConnection = new SqlConnection(DbConString))
+            {
+                SqlCommand sqlCommand = SqlCommandInstance(storedProcedure, sqlConnection);
+                SqlDataAdapter adpt = new SqlDataAdapter(sqlCommand); //using
+                
+                try
+                {
+                    DataSet ds = new DataSet();
+                    adpt.Fill(ds);
+                    return ds.Tables[0].ToEnumerable<T>();
+                }
+                catch (SqlException sqlEx)
+                {
+                    throw new ArgumentException("Some Error occured at database, if error in stored procedure. See inner exception for more detail exception." + storedProcedure, sqlEx);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get object by ID from table in database.
+        /// </summary>
+        /// <param name="byId">Id.</param>
+        /// <returns>Returns object.</returns>
+        public T GetByID(int byId)
+        {
+            if (byId == 0)
+                throw new ArgumentNullException("byId should not be 0");
+
+            string tableName = new T().GetType().Name;
+            string storedProcedure = "Get" + tableName + "ById";
+
+            using (SqlConnection sqlConnection = new SqlConnection(DbConString))
+            {
+                SqlCommand sqlCommand = SqlCommandInstance(storedProcedure, sqlConnection, new SqlParameter[] { new SqlParameter("Id", byId) });
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                try
+                {
+                    DataSet ds = new DataSet();
+                    sqlDataAdapter.Fill(ds);
+                    return ds.Tables[0].ToEnumerable<T>().ToList().SingleOrDefault(); 
+                }
+                catch (SqlException sqlEx)
+                {
+                    throw new ArgumentException("Some Error occured at database, if error in stored procedure: " + storedProcedure, sqlEx);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Implementation Update <see cref="ICRUD{T}.Update(T)"/>
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns>Returns updated elements.</returns>
+        public T Update(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException();
+
+            string tableName = new T().GetType().Name;
+            string storedProcedure = "Update" + tableName;
+
+            using (SqlConnection sqlConnection = new SqlConnection(DbConString))
+            {
+                SqlCommand sqlCommand = SqlCommandInstance(storedProcedure, sqlConnection);
+                sqlCommand.Parameters.AddRange(GetUpdateParameter(entity).ToArray());
+
+                SqlDataAdapter adpt = new SqlDataAdapter(sqlCommand);
+                DataSet ds = new DataSet();
+
+                try
+                {
+                    adpt.Fill(ds);
+                    return ds.Tables[0].ToEnumerable<T>().ToList().SingleOrDefault();
+                }
+                catch (SqlException sqlEx)
+                {
+                    throw new ArgumentException("Class Name and Table name must be same for this method. See inner exception", sqlEx);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creating sqlCommand.
+        /// </summary>
+        /// <param name="storedProcedure">Name of stored procedure.</param>
+        /// <param name="sqlConnection"> Sql connection sting</param>
+        /// <returns>return sql command</returns>
+        private SqlCommand SqlCommandInstance(string storedProcedure, SqlConnection sqlConnection)
+        {
+            SqlCommand sqlCommand = new SqlCommand(storedProcedure, sqlConnection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            return sqlCommand;
+        }
+
+        /// <summary>
+        /// Private method for get property from objects and add their to list for sqlParameters
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>returns list of sqlParameters.</returns>
+        private List<SqlParameter> GetUpdateParameter(object obj)
+        {
+            PropertyInfo[] fields = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var sqlParams = new List<SqlParameter>();
+            foreach (var f in fields)
+            {
+                sqlParams.Add(new SqlParameter(f.Name, f.GetValue(obj, null)));
+            }
+            return sqlParams;
+        }
+
+        /// <summary>
+        /// Creating sqlCommand.
+        /// </summary>
+        /// <param name="storedProcedure">Name of stored procedure.</param>
+        /// <param name="sqlConnection"> Sql connection sting</param>
+        /// <param name="sqlParamArr"> SqlParameters</param>
+        /// <returns>return sql command</returns>
+        private SqlCommand SqlCommandInstance(string storedProcedure, SqlConnection sqlConnection, SqlParameter[] sqlParamArr)
+        {
+            SqlCommand sqlCommand = SqlCommandInstance(storedProcedure, sqlConnection);
+            sqlCommand.Parameters.AddRange(sqlParamArr);
+            return sqlCommand;
+        }
+
+        /// <summary>
+        /// Private method for get property from objects and add their to list for sqlParameters
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>returns list of sqlParameters.</returns>
+        private List<SqlParameter> GetAddParameter(object obj)
+        {
+            PropertyInfo[] fields = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var sqlParams = new List<SqlParameter>();
+            foreach (var f in fields)
+            {
+                if (f.GetCustomAttributes(false).Length != 0)
+                {
+                    if (f.GetCustomAttributesData()[0].AttributeType.Name != "KeyAttribute") //f.GetCustomAttributes(false).Length == 0 && 
+                    {
+                        sqlParams.Add(new SqlParameter(f.Name, f.GetValue(obj, null)));
+                    }
+                }
+                else
+                {
+                    sqlParams.Add(new SqlParameter(f.Name, f.GetValue(obj, null)));
+                }
+            }
+            return sqlParams;
+        }
+
+    }
+}
